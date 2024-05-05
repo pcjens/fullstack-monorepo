6a904b872410daa727101c682273787eb2fc9c59
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2024-05-05 18:37:36 +0300

    Move hardcoded values for bmiCalculator to the package.json script

--- osa-9b/bmiCalculator.ts
+++ osa-9b/bmiCalculator.ts
@@ -1,4 +1,4 @@
-function calculateBmi(heightCm: number, weightKg: number): string {
+export function calculateBmi(heightCm: number, weightKg: number): string {
     const heightM = heightCm / 100.0;
     const bmi = weightKg / (heightM * heightM);
     if (bmi < 18.5)
@@ -9,5 +9,3 @@ function calculateBmi(heightCm: number, weightKg: number): string {
         return "Overweight";
     return "Obese";
 }
-
-console.log(calculateBmi(180, 74))
