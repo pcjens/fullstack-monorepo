be6fa1357dd133705317a60f490d450620a76fcd
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2024-05-05 19:40:40 +0300

    Implement ex. 9.5

--- osa-9b/src/index.ts
+++ osa-9b/src/index.ts
@@ -1,9 +1,33 @@
 import express from 'express';
+import qs from 'qs';
+import { calculateBmi } from './bmiCalculator';
 
 const app = express();
+app.set('query parser', (str: string) => qs.parse(str));
 
 app.get('/hello', (_req, res) => {
     res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+    const { weight, height } = req.query;
+    if (typeof weight != 'string' || typeof height != 'string') {
+        res.status(400).send({ error: 'malformatted parameters' });
+        return;
+    }
+
+    const heightCm = Number.parseFloat(height);
+    const weightKg = Number.parseFloat(weight);
+    if (isNaN(heightCm) || isNaN(weightKg)) {
+        res.status(400).send({ error: 'malformatted parameters' });
+        return;
+    }
+
+    res.send({
+        weight,
+        height,
+        bmi: calculateBmi(heightCm, weightKg),
+    });
 });
 
 const port = 3003;
