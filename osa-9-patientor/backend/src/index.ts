26ee9e832d4a7f2add24d57941f92676300f8b7c
Jens Pitkanen <jens.pitkanen@helsinki.fi>
2024-05-08 19:23:58 +0300

    Implement ex. 9.10

--- osa-9-patientor/backend/src/index.ts
+++ osa-9-patientor/backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import diagnosisRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
@@ -6,6 +7,8 @@ app.use(express.json());
 app.get('/api/ping', (_req, res) => {
     res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 const port = 3001;
 app.listen(port, () => {
